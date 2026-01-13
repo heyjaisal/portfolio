cbc330fca2fb5138d0bc601ae932ab19a870c8cf
heyjaisal <heyjaisal@gmail.com>
2026-01-13 18:05:27 +0530

    Home page update

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="w-full bg-black text-white font-inter md:h-screen md:overflow-hidden overflow-y-auto flex flex-col pt-20">
+    <div className="w-full bg-black text-white font-inter md:h-screen overflow-y-auto flex flex-col pt-20">
     
       <section className="grid grid-cols-1 md:grid-cols-2 shrink-0 md:grow-0 min-h-[200px] md:min-h-[25vh] items-center">
         <div className="flex items-center justify-center px-6">
           <div className="text-white text-3xl md:text-5xl lg:text-5xl font-poppins font-semibold leading-tight space-y-2 text-left w-full max-w-2xl mb-10 mt-5">
             <p className="text-yellow-300">Designer,</p>
-            <p>skil collector,</p>
+            <p>skill collector,</p>
             <p>backend builder.</p>
           </div>
         </div>
        
       </section>
- <section className="bg-grid px-4 py-8 md:py-10 mt-10">
+      <section className="bg-grid px-4 py-8 md:py-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {[
             {
@@ -39,7 +39,7 @@ export default function Home() {
               key={index}
               className="group space-y-2 rounded transform transition hover:text-yellow-300 duration-300 ease-in-out hover:scale-105"
             >
-              <Link to="/projects">
+             <Link to="/projects">
                 <img
                   src={item.img}
                   alt={item.title}
@@ -61,7 +61,7 @@ export default function Home() {
       </section>
 
     
-      <footer className="bg-black text-gray-400 text-sm px-6 py-4 border-t border-white/10 flex justify-between items-center shrink-0">
+      <footer className="bg-black text-gray-400 text-sm px-6 py-4 border-t border-white/10 flex justify-between items-center shrink-0 mt-auto">
         <div className="flex space-x-6">
           {[
             { name: "GitHub", url: "https://github.com/heyjaisal", external: true },
